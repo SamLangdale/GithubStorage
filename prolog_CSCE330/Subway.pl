d86a4e580d51763c21ed986e5b45a81fbfe0b0c4
SamLangdale <samlangdale13@gmail.com>
2024-09-10 14:43:51 -0400

    prolog

--- prolog_CSCE330/Subway.pl
+++ prolog_CSCE330/Subway.pl
@@ -8,5 +8,5 @@ leftOf(stgeorge,bay).
 leftOf(bathhurst,spadina).
 
 leftOf(X,Y) :- westOf(x,Y).
-leftOf(X,Z) :- leftof(X,Y),leftOf(Y,Z).
+leftOf(X,Z) :- leftOf(X,Y),leftOf(Y,Z).
 eastOf(X,Y) :- westOf(Y,X).
