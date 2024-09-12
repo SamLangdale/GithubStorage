3f912fa23a2d88789aba417683a568b9ac7307b0
SamLangdale <samlangdale13@gmail.com>
2024-09-11 20:24:11 -0400

    preliminary stuff

---
+++ Verilog/Seq_Multiplyer/Seq_Multyplyer.sv
@@ -0,0 +1,12 @@
+module seq_multiplier ( 
+    input logic [15:0] M,
+    input logic [15:0] Q,
+    input logic clk,
+    input logic reset,
+    input logic enable,
+    output logic [31:0] acc 
+
+
+)
+
+endmodule
