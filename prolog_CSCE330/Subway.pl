e34b5ec8f88508ad37127e6d3e3dfb64e526d3ba
SamLangdale <samlangdale13@gmail.com>
2024-09-13 19:30:22 -0400

    modified:   Subway.pl

--- prolog_CSCE330/Subway.pl
+++ prolog_CSCE330/Subway.pl
@@ -1,12 +1,19 @@
 
 % Subway example
 
+
+
+% 1. an example of a sentence that isnt entailed by the KB is bay is west of yonge.
+% 2. even though the statement is true there is no definition that defines what being right of something means.
+
 leftOf(bay,yonge).
 leftOf(spadina,stgeorge).
-leftOf(christie,bathhurst).
+leftOf(christie,bathurst).
 leftOf(stgeorge,bay).
-leftOf(bathhurst,spadina).
+leftOf(bathurst,spadina).
+
+westOf(X,Z) :- leftOf(X,Y), westOf(Y,Z).
 
 westOf(X,Y) :- leftOf(X,Y).
-leftOf(X,Z) :- leftOf(X,Y),leftOf(Y,Z).
-eastOf(X,Y) :- leftOf(Y,X).
+
+eastOf(X,Y) :- westOf(Y,X).   
