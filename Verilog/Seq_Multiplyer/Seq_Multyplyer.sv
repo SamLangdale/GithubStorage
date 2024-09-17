9b94eeb9cec863e6647e2eeac3bfc075151d686f
SamLangdale <samlangdale13@gmail.com>
2024-09-17 17:57:53 -0400

    got the seq multiplyer done

--- Verilog/Seq_Multiplyer/Seq_Multyplyer.sv
+++ Verilog/Seq_Multiplyer/Seq_Multyplyer.sv
@@ -5,8 +5,22 @@ module seq_multiplier (
     input logic reset,
     input logic enable,
     output logic [31:0] acc 
+);
 
+    integer i;
 
-)
+always @(posedge clk) begin
+    if (reset) begin // resets Accumulation to 0
+        acc[i] <= 32'd00000000000000000000000000000000;
+end
+    if (enable) begin
+        for (i=0; i<16; i = i+1) begin
+            acc[i] <= M[i]* Q[i];
+        end
+
+    end
+
+    end
+
 
 endmodule
