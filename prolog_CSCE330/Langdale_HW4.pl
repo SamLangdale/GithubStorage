5879269efa2af49e4cba8bfb53323dd5d86059ef
SamLangdale <samlangdale13@gmail.com>
2024-10-02 17:01:34 -0400

    new file:   prolog_CSCE330/Langdale_HW4.pl

---
+++ prolog_CSCE330/Langdale_HW4.pl
@@ -0,0 +1,8 @@
+/*
+1. 
+A. = tests for if two terms are identical
+B. =:= tests if two nunbers are equal
+C. is assignes the result on the right to the variable on the left
+*/
+
+% 2. france, switzerland, italy, belgium, germany, austria, the netherlands
