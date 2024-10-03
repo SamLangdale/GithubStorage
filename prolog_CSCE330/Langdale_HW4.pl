a323309603ecd7e47471846c87456c1e7f5ad6db
SamLangdale <samlangdale13@gmail.com>
2024-10-02 21:30:39 -0400

    modified:   prolog_CSCE330/Langdale_HW4.pl

--- prolog_CSCE330/Langdale_HW4.pl
+++ prolog_CSCE330/Langdale_HW4.pl
@@ -5,4 +5,21 @@ B. =:= tests if two nunbers are equal
 C. is assignes the result on the right to the variable on the left
 */
 
-% 2. france, switzerland, italy, belgium, germany, austria, the netherlands
+% 2. france, switzerland, italy, belgium, germany, austria, the netherlands red yellow orange 
+
+solution(F,S,I,B,G,A,N) :-
+    color(F), color(S), color(I), color(B), color(G), color(A), color(N),
+    \+ F=S, \+ S=I, \+ I=A, \+ B=G,\+ G=A,
+    \+ F=B, \+ S=A, \+ B=N, \+ G=N,
+    \+ F=G, \+ S=G,
+    \+ F=I.
+    color(red).
+    color(yellow).
+    color(blue).
+/*
+
+    ?- solution(F,S,I,B,G,A,N) .
+F = A, A = N, N = red,
+S = B, B = yellow,
+I = G, G = blue .
+*/
