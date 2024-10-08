211458df9875f48d2e4983a03a0a716b65c242a5
SamLangdale <samlangdale13@gmail.com>
2024-10-07 21:46:18 -0400

    new file:   prolog_CSCE330/Langdale_HW5.pl

---
+++ prolog_CSCE330/Langdale_HW5.pl
@@ -0,0 +1,47 @@
+
+% 6. A
+exactly_3([_,_,_]).
+/*
+ ?- exactly_3([1,2,3]).
+   Call: (12) exactly_3([1, 2, 3]) ? creep
+   Exit: (12) exactly_3([1, 2, 3]) ? creep
+true.
+*/
+
+%6. B
+at_least_3([_,_,_|_]).
+
+/* 
+ ?- at_least_3([1,2,3,4]).
+   Call: (12) at_least_3([1, 2, 3, 4]) ? creep
+   Exit: (12) at_least_3([1, 2, 3, 4]) ? creep
+true.
+*/
+
+%6. C
+at_most_3([]).
+at_most_3([_]).
+at_most_3([_,_]).
+at_most_3([_,_,_]).
+/*
+?- at_most_3([1,2,3,4]).
+   Call: (12) at_most_3([1, 2, 3, 4]) ? creep
+   Fail: (12) at_most_3([1, 2, 3, 4]) ? creep
+false.
+
+ at_most_3([1,2]).
+   Call: (12) at_most_3([1, 2]) ? creep
+   Exit: (12) at_most_3([1, 2]) ? creep
+true .
+*/
+
+%6 D.
+intersect(X,Y)
+/*
+
+*/
+
+
+
+
+
