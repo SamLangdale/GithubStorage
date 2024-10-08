ba014512c62549d0ee3aca1a28b4c2bc531c4c29
SamLangdale <samlangdale13@gmail.com>
2024-10-08 16:46:36 -0400

    modified:   prolog_CSCE330/Langdale_HW5.pl

--- prolog_CSCE330/Langdale_HW5.pl
+++ prolog_CSCE330/Langdale_HW5.pl
@@ -58,7 +58,67 @@ true.
 
 */
 % 6. E
-all_intersect(Z,Y,X)
+
+all_intersect([],_). % base case
+all_intersect([C|L],B) :- intersect(B,C), all_intersect(L,B).  
+
+/*
+?- all_intersect([[1,2,3],[1,2,5],[5,4,6]],[3,4]).
+   Call: (12) all_intersect([[1, 2, 3], [1, 2, 5], [5, 4, 6]], [3, 4]) ? creep
+   Call: (13) intersect([3, 4], [1, 2, 3]) ? creep
+   Call: (14) lists:member(_20020, [3, 4]) ? creep
+   Exit: (14) lists:member(3, [3, 4]) ? creep
+   Call: (14) lists:member(3, [1, 2, 3]) ? creep
+   Exit: (14) lists:member(3, [1, 2, 3]) ? creep
+   Exit: (13) intersect([3, 4], [1, 2, 3]) ? creep
+   Call: (13) all_intersect([[1, 2, 5], [5, 4, 6]], [3, 4]) ? creep
+   Call: (14) intersect([3, 4], [1, 2, 5]) ? creep
+   Call: (15) lists:member(_25740, [3, 4]) ? creep
+   Exit: (15) lists:member(3, [3, 4]) ? creep
+   Call: (15) lists:member(3, [1, 2, 5]) ? creep
+   Fail: (15) lists:member(3, [1, 2, 5]) ? creep
+   Redo: (15) lists:member(_25740, [3, 4]) ? creep
+   Exit: (15) lists:member(4, [3, 4]) ? creep
+   Call: (15) lists:member(4, [1, 2, 5]) ? creep
+   Fail: (15) lists:member(4, [1, 2, 5]) ? creep
+   Fail: (14) intersect([3, 4], [1, 2, 5]) ? creep
+   Fail: (13) all_intersect([[1, 2, 5], [5, 4, 6]], [3, 4]) ? creep
+   Redo: (14) lists:member(_20020, [3, 4]) ? creep
+   Exit: (14) lists:member(4, [3, 4]) ? creep
+   Call: (14) lists:member(4, [1, 2, 3]) ? creep
+   Fail: (14) lists:member(4, [1, 2, 3]) ? creep
+   Fail: (13) intersect([3, 4], [1, 2, 3]) ? creep
+   Fail: (12) all_intersect([[1, 2, 3], [1, 2, 5], [5, 4, 6]], [3, 4]) ? creep
+false.
+
+all_intersect([[1,2,3],[5,4,6]],[3,4]).
+   Call: (12) all_intersect([[1, 2, 3], [5, 4, 6]], [3, 4]) ? creep
+   Call: (13) intersect([3, 4], [1, 2, 3]) ? creep
+   Call: (14) lists:member(_43288, [3, 4]) ? creep
+   Exit: (14) lists:member(3, [3, 4]) ? creep
+   Call: (14) lists:member(3, [1, 2, 3]) ? creep
+   Exit: (14) lists:member(3, [1, 2, 3]) ? creep
+   Exit: (13) intersect([3, 4], [1, 2, 3]) ? creep
+   Call: (13) all_intersect([[5, 4, 6]], [3, 4]) ? creep
+   Call: (14) intersect([3, 4], [5, 4, 6]) ? creep
+   Call: (15) lists:member(_49008, [3, 4]) ? creep
+   Exit: (15) lists:member(3, [3, 4]) ? creep
+   Call: (15) lists:member(3, [5, 4, 6]) ? creep
+   Fail: (15) lists:member(3, [5, 4, 6]) ? creep
+   Redo: (15) lists:member(_49008, [3, 4]) ? creep
+   Exit: (15) lists:member(4, [3, 4]) ? creep
+   Call: (15) lists:member(4, [5, 4, 6]) ? creep
+   Exit: (15) lists:member(4, [5, 4, 6]) ? creep
+   Exit: (14) intersect([3, 4], [5, 4, 6]) ? creep
+   Call: (14) all_intersect([], [3, 4]) ? creep
+   Exit: (14) all_intersect([], [3, 4]) ? creep
+   Exit: (13) all_intersect([[5, 4, 6]], [3, 4])
+ ? creep
+   Exit: (12) all_intersect([[1, 2, 3], [5, 4, 6]], [3, 4])
+ ? creep
+true .
+
+*/
 
 
 
