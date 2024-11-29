ce3d8391f156786fa5ecbce0b11a394eb4ee39f2
SamLangdale <samlangdale13@gmail.com>
2024-11-29 15:03:12 -0500

    new file:   Haskell/Langdale_hw10.hs

---
+++ Haskell/Langdale_hw10.hs
@@ -0,0 +1,26 @@
+pyths :: Int -> [(Int,Int,Int)]
+
+pyths n = [(x,y,z) | x <- [1..n], y <- [1..n], z <- [1..n], x*x + y*y == z*z]
+
+{-ghci> :l C:\Users\samla\Desktop\GithubStorage\Haskell\Langdalehw10.hs
+[1 of 2] Compiling Main             ( C:\Users\samla\Desktop\GithubStorage\Haskell\Langdalehw10.hs, interpreted )
+Ok, one module loaded.
+ghci> pyths 10
+[(3,4,5),(4,3,5),(6,8,10),(8,6,10)]
+ghci> pyths 15
+[(3,4,5),(4,3,5),(5,12,13),(6,8,10),(8,6,10),(9,12,15),(12,5,13),(12,9,15)]
+ghci>
+-}
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], sum (factors x)-x == x]
+
+{-ghci> :l C:\Users\samla\Desktop\GithubStorage\Haskell\Langdalehw10.hs
+[1 of 2] Compiling Main             ( C:\Users\samla\Desktop\GithubStorage\Haskell\Langdalehw10.hs, interpreted )
+Ok, one module loaded.
+ghci> perfects 500
+[6,28,496]
+ghci> perfects 10000
+[6,28,496,8128]
+-}
