01e05f73c0e34bdcbe3bf4b51a0707c826b70983
SamLangdale <samlangdale13@gmail.com>
2024-12-09 23:42:26 -0500

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   Acum.pl
    
     Changes not staged for commit:
    	modified:   ../Haskell/Langdale_hw10.hs
    	modified:   ../mergeReal/src/main.rs
    	modified:   ../mergeReal/target/.rustc_info.json
    	modified:   ../mergeReal/target/debug/.fingerprint/mergeReal-e82840375a189af6/bin-mergeReal
    	deleted:    ../mergeReal/target/debug/deps/mergeReal.exe
    	deleted:    ../mergeReal/target/debug/deps/mergeReal.pdb
    
     Untracked files:
    	../Haskell/Exam_Study.hs
    	../mergeReal/target/debug/.fingerprint/mergeReal-e82840375a189af6/output-bin-mergeReal
    	../mergeReal/target/debug/incremental/mergeReal-0jva2iwp41dp9/s-h2jcl3tkf9-0swomvs-working/
    	../mergeReal/target/debug/incremental/mergeReal-0jva2iwp41dp9/s-h2jcl3tkf9-0swomvs.lock
    	../mergeReal/target/debug/incremental/mergeReal-26elxleehzm5h/s-h2jcl3ar5j-00s24hm-working/
    	../mergeReal/target/debug/incremental/mergeReal-26elxleehzm5h/s-h2jcl3ar5j-00s24hm.lock
    	../mergeReal/target/debug/incremental/mergeReal-3agf3o4kwhsy7/s-h2jcl3ar6g-1dtfwap-working/
    	../mergeReal/target/debug/incremental/mergeReal-3agf3o4kwhsy7/s-h2jcl3ar6g-1dtfwap.lock

--- prolog_CSCE330/Acum.pl
+++ prolog_CSCE330/Acum.pl
@@ -8,3 +8,20 @@ list_length([_|Tail], Length) :-
 
    power_of_two(128, Solution) :-
     findall(Pow, (between(0, 10, Pow), 2**Pow =:= 128), Solution).
+
+
+factorial(N,R) :- factorial(N,1,R).
+factorial(0,R,R).
+factorial(N,Acc,R) :-
+    NewN is N-1,
+    NewAcc is Acc * N,
+    factorial(NewN,NewAcc,R).
+
+pow(A, B, R) :- pow(A, B, R, 1). % Ac
+pow(A, 0, R, R).
+pow(A, B, R, Ac) :-
+    B > 0,
+    B2 is B - 1,
+    Ac2 is Ac * A,
+    pow(A, B2, R, Ac2).
+
